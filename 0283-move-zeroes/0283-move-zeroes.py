c3cb4b6481945c838106cc3d3cacb62ca20151d6
shivam dubey <shivamdubey6122@gmail.com>
2026-07-14 10:52:24 +0530

    Time: 4 ms (79.57%), Space: 13.6 MB (48.39%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        k = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[k] = nums[i]
+                k += 1
+
+        while k < len(nums):
+            nums[k] = 0
+            k += 1    
+
+
+
+        
