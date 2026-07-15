9b3baaf98b35a86457cff9f9d2481f1540e53ef7
shivam dubey <shivamdubey6122@gmail.com>
2026-07-15 12:18:36 +0530

    Time: 271 ms (6.46%), Space: 12.9 MB (62.35%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        cleaned=""
+        for ch in s:
+            if ch.isalnum():
+                cleaned+=ch.lower()
+        return cleaned==cleaned[::-1]        
+           
+        
